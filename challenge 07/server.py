4687a6a34727ee667092a9a3658fc62518e93d80
T J Navaneeth Krishnan <143018158+navneethtj@users.noreply.github.com>
2024-05-14 18:50:30 +0530

    Update server.py

--- challenge 07/server.py
+++ challenge 07/server.py
@@ -14,12 +14,6 @@ def run_server():
 
     StartTcpServer(context, address=("127.0.0.1", 5020))
 
-    print("Values received and sent by the server:")
-    print("DI:", context[0x01].di)
-    print("CO:", context[0x01].co)
-    print("HR:", context[0x01].hr)
-    print("IR:", context[0x01].ir)
-
 if __name__ == "__main__":
     print("Modbus server started on localhost port 5020")
     run_server()
